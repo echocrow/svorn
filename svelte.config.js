440cfdca7ae7d7cb51de426b3a70d8f37548b356
Crow <5488190+echocrow@users.noreply.github.com>
2021-12-13 01:21:07 +0100

    Merge with Svelte skeleton template

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -5,8 +6,12 @@ const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
+
   compilerOptions: { immutable: true },
+
   kit: {
+    adapter: adapter(),
+
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
   },
