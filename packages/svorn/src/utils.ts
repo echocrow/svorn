5eef40e2ba0c7c0185384e70ef72ae3f5e10254c
Crow <5488190+echocrow@users.noreply.github.com>
2022-02-12 23:47:17 +0100

    Remove unused utils

--- packages/svorn/src/utils.ts
+++ packages/svorn/src/utils.ts
@@ -1,9 +1,4 @@
-import type {
-  ObservedValueOf,
-  Observer,
-  Subscribable,
-  Unsubscribable,
-} from 'rxjs'
+import type { Observer } from 'rxjs'
 
 import type { FamilyKey, InteropObserver } from './types'
 
@@ -12,51 +7,6 @@ export const isEmpty = (
 ): boolean => {
   for (const _ in obj) return false
   return true
-}
-
-export const areSetsEqual = <T>(a: Set<T>, b: Set<T>): boolean => {
-  if (a.size !== b.size) return false
-  for (const v of a) if (!b.has(v)) return false
-  return true
-}
-
-export const mapContains = (
-  outer: Map<unknown, unknown>,
-  inner: Map<unknown, unknown>,
-): boolean => {
-  if (outer.size < inner.size) return false
-  for (const [k, v] of inner.entries())
-    if (!outer.has(k) || !Object.is(outer.get(k), v)) return false
-  return true
-}
-
-export const zipSetArray = <K, V>(ks: Set<K>, vs: V[]): Map<K, V> => {
-  if (ks.size !== vs.length) throw new Error('todo: set vs array size mismatch')
-  const m = new Map<K, V>()
-  let i = 0
-  for (const k of ks) {
-    m.set(k, vs[i]!) // eslint-disable-line @typescript-eslint/no-non-null-assertion
-    i++
-  }
-  return m
-}
-
-export const requireInstantValue = <S extends Subscribable<ObservedValueOf<S>>>(
-  source: S,
-): readonly [value: ObservedValueOf<S>, subscription: Unsubscribable] => {
-  let value: [ObservedValueOf<S>] | undefined = undefined
-  let err: Error | undefined
-  const subscription = source.subscribe({
-    next: (v) => (value = [v]),
-    error: (e) => (err = e),
-  })
-  if (!value || err) {
-    if (!err) err = new Error('todo: value not received')
-    subscription.unsubscribe()
-    throw err
-  }
-  const v = value[0]
-  return [v, subscription]
 }
 
 export const stringify = (key: FamilyKey): string => {
