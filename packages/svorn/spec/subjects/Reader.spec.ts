9afc4ae507fec36caa67c23672dc162e435c8bc0
Crow <5488190+echocrow@users.noreply.github.com>
2022-02-16 01:02:41 +0100

    Add Readable

---
+++ packages/svorn/spec/subjects/Reader.spec.ts
@@ -0,0 +1,122 @@
+import { BehaviorSubject } from 'rxjs'
+import { describeReadable, runTestScheduler } from 'spec/helpers'
+import Reader, { readable } from 'src/subjects/Reader'
+
+const noop = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+
+describe('Reader', () => {
+  test.each(['', '0', 0, 123, true, 'def'] as const)(
+    'emits initial value synchronously (%j)',
+    (v) => {
+      runTestScheduler(({ expectObservable }) => {
+        expectObservable(new Reader(v, noop)).toBe('d', { d: v })
+      })
+    },
+  )
+
+  describe('with set callback', () => {
+    describeReadable(
+      () => {
+        const s = new BehaviorSubject('__BUCKET-DEFAULT__')
+        const r = new Reader('__DEFAULT__', (set) => {
+          const sub = s.subscribe((v) => set(v))
+          return () => sub.unsubscribe()
+        })
+        return [r, s]
+      },
+      { skipCompletion: true, skipError: true },
+    )
+  })
+
+  describe('with set object', () => {
+    describeReadable(() => {
+      const s = new BehaviorSubject('__BUCKET-DEFAULT__')
+      const r = new Reader('__DEFAULT__', ({ next, error, complete }) => {
+        const sub = s.subscribe({ next, error, complete })
+        return () => sub.unsubscribe()
+      })
+      return [r, s]
+    })
+  })
+
+  it('only calls set on first new subscription', () => {
+    const init = jest.fn()
+
+    const r = new Reader('0', init)
+    expect(init).toBeCalledTimes(0)
+
+    const s0a = r.subscribe(noop)
+    expect(init).toBeCalledTimes(1)
+    s0a.unsubscribe()
+    expect(init).toBeCalledTimes(1)
+
+    const s1a = r.subscribe(noop)
+    expect(init).toBeCalledTimes(2)
+    const s1b = r.subscribe(noop)
+    expect(init).toBeCalledTimes(2)
+    s1b.unsubscribe()
+    s1a.unsubscribe()
+    expect(init).toBeCalledTimes(2)
+
+    const s2a = r.subscribe(noop)
+    expect(init).toBeCalledTimes(3)
+    const s2b = r.subscribe(noop)
+    expect(init).toBeCalledTimes(3)
+    s2a.unsubscribe()
+    s2b.unsubscribe()
+    expect(init).toBeCalledTimes(3)
+  })
+
+  it('only calls destroy when last subscription ended', () => {
+    const destroy = jest.fn()
+    const init = () => destroy
+
+    const r = new Reader('0', init)
+    expect(destroy).toBeCalledTimes(0)
+
+    const s0a = r.subscribe(noop)
+    expect(destroy).toBeCalledTimes(0)
+    s0a.unsubscribe()
+    expect(destroy).toBeCalledTimes(1)
+
+    const s1a = r.subscribe(noop)
+    const s1b = r.subscribe(noop)
+    expect(destroy).toBeCalledTimes(1)
+    s1a.unsubscribe()
+    expect(destroy).toBeCalledTimes(1)
+    s1b.unsubscribe()
+    expect(destroy).toBeCalledTimes(2)
+
+    const s2a = r.subscribe(noop)
+    const s2b = r.subscribe(noop)
+    expect(destroy).toBeCalledTimes(2)
+    s2b.unsubscribe()
+    expect(destroy).toBeCalledTimes(2)
+    s2a.unsubscribe()
+    expect(destroy).toBeCalledTimes(3)
+  })
+})
+
+describe('readable', () => {
+  it('returns Reader', () => {
+    expect(readable('0', noop)).toBeInstanceOf(Reader)
+  })
+
+  test.each(['', '0', 123, true])('keeps initial value (%j)', (v) => {
+    runTestScheduler(({ expectObservable }) => {
+      expectObservable(new Reader(v, noop)).toBe('d', { d: v })
+    })
+  })
+
+  it('keeps the init callback', () => {
+    const init = jest.fn()
+    const r = readable('0', init)
+    expect(init).toBeCalledTimes(0)
+
+    const sub = r.subscribe(noop)
+    expect(init).toBeCalledTimes(1)
+
+    sub.unsubscribe()
+    expect(init).toBeCalledTimes(1)
+  })
+})
---
+++ packages/svorn/src/subjects/Reader.ts
@@ -0,0 +1,52 @@
+import {
+  type Observer,
+  type Subscriber,
+  finalize,
+  Observable,
+  share,
+} from 'rxjs'
+import DerivedReader from 'src/helpers/DerivedReader'
+import defaultWith from 'src/operators/defaultWith'
+
+import type { Readable } from '../types'
+
+interface ReaderSet<V> extends Observer<V> {
+  (value: V): void
+}
+
+type ReaderInit<V> = (set: ReaderSet<V>) => (() => void) | void
+
+class Reader<V> extends DerivedReader<V> implements Readable<V> {
+  #destroy: (() => void) | void = undefined
+  #src: Observable<V>
+
+  constructor(initialValue: V, init: ReaderInit<V>) {
+    super()
+    this.#src = new Observable<V>((subscriber) => {
+      const set = (value: V) => subscriber.next(value)
+      set.next = (value: V) => subscriber.next(value)
+      set.error = (err: unknown) => subscriber.error(err)
+      set.complete = () => subscriber.complete()
+
+      this.#destroy = init(set)
+    }).pipe(
+      finalize(() => this.destroy()),
+      defaultWith(initialValue),
+      share(),
+    )
+  }
+
+  protected _subscribe(subscriber: Subscriber<V>) {
+    return this.#src.subscribe(subscriber)
+  }
+
+  private destroy() {
+    this.#destroy?.()
+    this.#destroy = undefined
+  }
+}
+
+export const readable = <V>(initialValue: V, init: ReaderInit<V>): Reader<V> =>
+  new Reader(initialValue, init)
+
+export default Reader
