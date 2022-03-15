dc726a0cbe33db2ba4bcb2c27840afe80e69ac28
Crow <5488190+echocrow@users.noreply.github.com>
2022-03-16 00:57:54 +0100

    Make Jest config reusable

--- packages/svorn/jest.config.js
+++ packages/svorn/jest.config.js
@@ -1,9 +1,12 @@
+import baseConfig from '../../jest.config.js'
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+const config = {
+  ...baseConfig,
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
     '^spec/(.*)$': '<rootDir>/spec/$1',
   },
 }
+
+export default config
