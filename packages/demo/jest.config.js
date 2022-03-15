fed12aa7f7a1f2466c2c03d83818ea4b50a6b603
Crow <5488190+echocrow@users.noreply.github.com>
2022-03-16 00:58:17 +0100

    Add Jest to demo

---
+++ packages/demo/jest.config.js
@@ -0,0 +1,11 @@
+import baseConfig from '../../jest.config.js'
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const config = {
+  ...baseConfig,
+  moduleNameMapper: {
+    '^\\$lib/(.*)$': '<rootDir>/src/lib/$1',
+  },
+}
+
+export default config
