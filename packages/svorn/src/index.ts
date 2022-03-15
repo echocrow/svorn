4275c1db340a48cdf490f7d98a8e76ffaae22138
Crow <5488190+echocrow@users.noreply.github.com>
2022-03-15 03:13:09 +0100

    Make Svorn util operator reusable

--- packages/svorn/src/index.ts
+++ packages/svorn/src/index.ts
@@ -1,3 +1,4 @@
+export { default as switchCombineLatest } from './operators/switchCombineLatest'
 export { default as derived, Deriver } from './stores/derived'
 export { default as derivedFamily, DeriverFamily } from './stores/derivedFamily'
 export { default as writable, Writer } from './stores/writable'
