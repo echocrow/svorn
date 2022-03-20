f7b42ec476f6b7cd18f62574fe858b1e3194b550
Crow <5488190+echocrow@users.noreply.github.com>
2022-03-20 18:30:14 +0100

    Fix process undefined in browser

--- packages/demo/src/lib/utils.ts
+++ packages/demo/src/lib/utils.ts
@@ -8,4 +8,5 @@ export const asNumber = (val: string | number): number =>
   typeof val === 'number' ? val ?? 0 : !val ? 0 : parseInt(val, 10) ?? 0
 
 export const IS_DEV_ENV =
-  process?.env?.NODE_ENV === 'development' || process?.env?.NODE_ENV === 'test'
+  typeof process !== 'undefined' &&
+  (process.env?.NODE_ENV === 'development' || process.env?.NODE_ENV === 'test')
